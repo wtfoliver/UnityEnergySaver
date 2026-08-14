d61ef3feb288849d1969f3888a5948f85a1e4c7e
Oliver <oliver@walkthefrog.com>
2026-08-14 10:58:17 +0200

    FrameRateAction update
    - FrameRateAction now reacts differently to standalone builds or handheld builds

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.2.0
+- Updated FrameRateAction to respond differently to standalone builds or handheld builds
+
 ## 0.1.0
 Release test version
 
--- EnergySaving/EnergyActions/FrameRateAction.cs
+++ EnergySaving/EnergyActions/FrameRateAction.cs
@@ -1,12 +1,40 @@
 using UnityEngine;
 
 /// <summary>
-/// In order to FPS limiting to actually work, VSync needs to be set to 0.
+/// In order to FPS limiting to actually work, VSync needs to be set to 0. 
 /// Source: https://docs.unity3d.com/ScriptReference/QualitySettings-vSyncCount.html
+/// 
+/// Unfortunately this introduces additional cost to sync CPU and GPU.
+/// Therefore for standalone applications QualitySettings.vSyncCount is increased, instead of changing Application.targetFrameRate.
+/// 
+/// You can always choose to use RenderingIntervalAction additionaly or instead of this class.
 /// </summary>
 [RequireComponent(typeof(EnergySaver))]
 public class FrameRateAction : MonoBehaviour, IEnergyAction
 {
+#if UNITY_STANDALONE_WIN || UNITY_STANDALONE_LINUX || UNITY_STANDALONE_OSX
+    int _initialVSyncCount;
+
+    void Awake()
+    {
+        _initialVSyncCount = QualitySettings.vSyncCount;
+    }
+
+    public void Apply(EnergyProfile profile)
+    {
+        if (profile == null) return;
+
+        float refreshRate = (float)Screen.currentResolution.refreshRateRatio.value;
+        int divisor = Mathf.Clamp(Mathf.RoundToInt(refreshRate / profile.MaxFps), 1, 4);
+
+        QualitySettings.vSyncCount = divisor;
+    }
+
+    public void OnDisable()
+    {
+        QualitySettings.vSyncCount = _initialVSyncCount;
+    }
+#else
     public void Apply(EnergyProfile profile)
     {
         if (profile == null) return;
@@ -14,4 +42,5 @@ public class FrameRateAction : MonoBehaviour, IEnergyAction
     }
 
     public void OnDisable() { }
+#endif
 }
